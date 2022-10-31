d5038cc68dacd8fac3c208f346014b68748d2028
haritshukri <mharitshukri@gmail.com>
2022-10-31 01:01:07 +0000

    compile lab1

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
-#include <stdio.h> 
-int main(void) { 
-/* This is my first program in C */ 
-printf(“Hello World!”); 
-printf(“I Love C”); 
-return (0); 
-} 
+#include <stdio.h>
+int main(void){
+/*This is my first C program*/
+printf("Hello World!");
+printf("I love C");
+return(0);
+}
